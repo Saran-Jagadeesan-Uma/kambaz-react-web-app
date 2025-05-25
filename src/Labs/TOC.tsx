f5173d2840e7615d5c47c4c031297a9d9805546d
Saran J U <122967632+Saran-Jagadeesan-Uma@users.noreply.github.com>
2025-05-25 19:52:39 +0400

    push lab

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,41 +1,62 @@
 import Nav from "react-bootstrap/Nav";
-import { NavLink } from "react-router-dom";
-
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
 export default function TOC() {
+  const { pathname } = useLocation();
   return (
-    <Nav variant="pills">
+    <Nav variant="pills" id="wd-toc">
       <Nav.Item>
-        <Nav.Link as={NavLink} to="/Labs" end>
-          Labs
-        </Nav.Link>
+        {" "}
+        <Nav.Link
+          as={Link}
+          to="/Labs/Lab1"
+          id="wd-a1"
+          active={pathname.includes("Lab1")}
+        >
+          {" "}
+          Lab 1{" "}
+        </Nav.Link>{" "}
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={NavLink} to="/Labs/Lab1">
-          Lab 1
-        </Nav.Link>
+        {" "}
+        <Nav.Link
+          as={Link}
+          to="/Labs/Lab2"
+          id="wd-a2"
+          active={pathname.includes("Lab2")}
+        >
+          {" "}
+          Lab 2{" "}
+        </Nav.Link>{" "}
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={NavLink} to="/Labs/Lab2">
-          Lab 2
-        </Nav.Link>
+        {" "}
+        <Nav.Link
+          as={Link}
+          to="/Labs/Lab3"
+          id="wd-a3"
+          active={pathname.includes("Lab3")}
+        >
+          {" "}
+          Lab 3{" "}
+        </Nav.Link>{" "}
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={NavLink} to="/Labs/Lab3">
-          Lab 3
-        </Nav.Link>
+        {" "}
+        <Nav.Link as={Link} to="/Kambaz" id="wd-a3">
+          {" "}
+          Kambaz{" "}
+        </Nav.Link>{" "}
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={NavLink} to="/Kambaz">
-          Kambaz
-        </Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
+        {" "}
         <Nav.Link
           href="https://github.com/Saran-Jagadeesan-Uma"
           target="_blank"
         >
-          GitHub
-        </Nav.Link>
+          {" "}
+          My GitHub{" "}
+        </Nav.Link>{" "}
       </Nav.Item>
     </Nav>
   );

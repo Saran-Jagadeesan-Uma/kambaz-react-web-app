77f4995dad7ba23db7d2c66e646f49518fe3e84c
Saran J U <122967632+Saran-Jagadeesan-Uma@users.noreply.github.com>
2025-05-26 11:18:24 +0400

    fixes

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,99 +1,42 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 
 export default function CourseNavigation() {
+  const { cid } = useParams();
+
+  const links = [
+    "Home",
+    "Modules",
+    "Piazza",
+    "Zoom",
+    "Assignments",
+    "Quizzes",
+    "Grades",
+    "People",
+  ];
+
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <NavLink
-        to="/Kambaz/Courses/1234/Home"
-        id="wd-course-home-link"
-        className={({ isActive }) =>
-          `list-group-item border border-0 ${isActive ? "active" : ""}`
-        }
-        style={({ isActive }) => ({
-          color: isActive ? "black" : "red",
-        })}
-      >
-        {" "}
-        Home{" "}
-      </NavLink>
-      <NavLink
-        to="/Kambaz/Courses/1234/Modules"
-        id="wd-course-modules-link"
-        className={({ isActive }) =>
-          `list-group-item border border-0 ${isActive ? "active" : ""}`
-        }
-        style={({ isActive }) => ({
-          color: isActive ? "black" : "red",
-        })}
-      >
-        {" "}
-        Modules{" "}
-      </NavLink>
-      <NavLink
-        to="/Kambaz/Courses/1234/Piazza"
-        id="wd-course-piazza-link"
-        className={({ isActive }) =>
-          `list-group-item border border-0 ${isActive ? "active" : ""}`
-        }
-        style={({ isActive }) => ({
-          color: isActive ? "black" : "red",
-        })}
-      >
-        {" "}
-        Piazza{" "}
-      </NavLink>
-      <NavLink
-        to="/Kambaz/Courses/1234/Zoom"
-        id="wd-course-zoom-link"
-        className={({ isActive }) =>
-          `list-group-item border border-0 ${isActive ? "active" : ""}`
-        }
-        style={({ isActive }) => ({
-          color: isActive ? "black" : "red",
-        })}
-      >
-        {" "}
-        Zoom{" "}
-      </NavLink>
-      <NavLink
-        to="/Kambaz/Courses/1234/Assignments"
-        id="wd-course-quizzes-link"
-        className={({ isActive }) =>
-          `list-group-item border border-0 ${isActive ? "active" : ""}`
-        }
-        style={({ isActive }) => ({
-          color: isActive ? "black" : "red",
-        })}
-      >
-        {" "}
-        Assignments{" "}
-      </NavLink>
-      <NavLink
-        to="/Kambaz/Courses/1234/Quizzes"
-        id="wd-course-assignments-link"
-        className={({ isActive }) =>
-          `list-group-item border border-0 ${isActive ? "active" : ""}`
-        }
-        style={({ isActive }) => ({
-          color: isActive ? "black" : "red",
-        })}
-      >
-        {" "}
-        Quizzes{" "}
-      </NavLink>
-      <NavLink
-        to="/Kambaz/Courses/1234/People"
-        id="wd-course-people-link"
-        className={({ isActive }) =>
-          `list-group-item border border-0 ${isActive ? "active" : ""}`
-        }
-        style={({ isActive }) => ({
-          color: isActive ? "black" : "red",
-        })}
-      >
-        {" "}
-        People{" "}
-      </NavLink>
+      {links.map((link) => {
+        const to = `/Kambaz/Courses/${cid}/${link}`;
+
+        const id = `wd-course-${link.toLowerCase()}-link`;
+
+        return (
+          <NavLink
+            key={link}
+            to={to}
+            id={id}
+            className={({ isActive }) =>
+              `list-group-item border border-0 ${isActive ? "active" : ""}`
+            }
+            style={({ isActive }) => ({
+              color: isActive ? "black" : "red",
+            })}
+          >
+            {link}
+          </NavLink>
+        );
+      })}
     </div>
   );
 }

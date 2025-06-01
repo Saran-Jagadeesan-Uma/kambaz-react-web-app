b129c9cc1a8d68d36ce2d9a9585f7082bbfa1f3a
Saran J U <122967632+Saran-Jagadeesan-Uma@users.noreply.github.com>
2025-06-01 11:18:55 +0400

    Initial Lab

---
+++ src/Labs/Lab4/index.tsx
@@ -0,0 +1,33 @@
+import ArrayStateVariable from "./ArrayStateVariable";
+import BooleanStateVariables from "./BooleanStateVariables";
+import ClickEvent from "./clickEvent";
+import Counter from "./Counter";
+import DateStateVariable from "./DateStateVariables";
+import EventObject from "./EventObject";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./passingfunctions";
+import StringStateVariables from "./StringStateVariables";
+
+export default function lab4() {
+  function sayHello() {
+    alert("Hello");
+  }
+
+  return (
+    <div id="wd-lab3" className="container">
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent/>
+    </div>
+  );
+}

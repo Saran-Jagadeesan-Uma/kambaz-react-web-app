58a6a7ad7689409e81271ca98c971e68a9b46de8
Saran J U <122967632+Saran-Jagadeesan-Uma@users.noreply.github.com>
2025-06-11 02:14:49 +0530

    assignments added

--- src/Kambaz/Courses/Assignments/client.ts
+++ src/Kambaz/Courses/Assignments/client.ts
@@ -1,27 +1,28 @@
+// src/Kambaz/Assignments/client.ts
 import axios from "axios";
 
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
-const ASSIGNMENTS_URL = `${REMOTE_SERVER}/api/assignments`;
+const ASSIGNMENTS_API = `${REMOTE_SERVER}/api/assignments`;
 
-export const findAllAssignments = async () => {
-  const { data } = await axios.get(ASSIGNMENTS_URL);
+export const createAssignment = async (assignment: any) => {
+  const { data } = await axios.post(ASSIGNMENTS_API, assignment);
   return data;
 };
 
-export const deleteAssignment = async (assignmentId: string) => {
-  const { data } = await axios.delete(`${ASSIGNMENTS_URL}/${assignmentId}`);
+export const findAllAssignments = async () => {
+  const { data } = await axios.get(ASSIGNMENTS_API);
   return data;
 };
 
 export const updateAssignment = async (assignment: any) => {
   const { data } = await axios.put(
-    `${ASSIGNMENTS_URL}/${assignment._id}`,
+    `${ASSIGNMENTS_API}/${assignment._id}`,
     assignment
   );
   return data;
 };
 
-export const createAssignment = async (assignment: any) => {
-  const { data } = await axios.post(ASSIGNMENTS_URL, assignment);
+export const deleteAssignment = async (assignmentId: string) => {
+  const { data } = await axios.delete(`${ASSIGNMENTS_API}/${assignmentId}`);
   return data;
 };
